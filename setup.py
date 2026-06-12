0d9bacffbfd666f63a5a7ae7d2217ad31d08a0b6
MUGUNDAN <mugunthan.selvanayagam@multicorewareinc.com>
2026-06-12 11:52:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -158,20 +158,20 @@ def main():
 
     if os.name != "nt":
             ci_cmake_generator = ["-G", "Unix Makefiles"]
-        else:
-            try:
-                vswhere = r"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
-                toolset = subprocess.check_output(
-                    [vswhere, "-latest", "-prerelease", "-property", "installationVersion"],
-                    text=True,
-                ).strip().split(".")[0]
-                year = subprocess.check_output(
-                    [vswhere, "-latest", "-prerelease", "-property", "catalog_productLineVersion"],
-                    text=True,
-                ).strip()
-                ci_cmake_generator = ["-G", f"Visual Studio {toolset} {year}"]
-            except Exception:
-                ci_cmake_generator = ["-G", "Visual Studio 17 2022"]
+    else:
+        try:
+            vswhere = r"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+            toolset = subprocess.check_output(
+                [vswhere, "-latest", "-prerelease", "-property", "installationVersion"],
+                text=True,
+            ).strip().split(".")[0]
+            year = subprocess.check_output(
+                [vswhere, "-latest", "-prerelease", "-property", "catalog_productLineVersion"],
+                text=True,
+            ).strip()
+            ci_cmake_generator = ["-G", f"Visual Studio {toolset} {year}"]
+        except Exception:
+            ci_cmake_generator = ["-G", "Visual Studio 17 2022"]
 
     cmake_args = (
         (ci_cmake_generator if is_CI_build else [])
